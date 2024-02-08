c0c6f9ffd2b151c964c46f4cd7920db8f4d0f6fe
Hepteto <136929258+Hepteto@users.noreply.github.com>
2024-02-08 18:52:10 -0300

    Update sequelize.js

--- sequelize.js
+++ sequelize.js
@@ -2,8 +2,9 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: './database.sqlite',
+  storage: ':memory:', // Alterado para SQLite na memória
   logging: false // Desativa os logs do Sequelize
 });
+
 
 module.exports = sequelize;
